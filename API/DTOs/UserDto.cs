4494d9c5de7895fbbd35b2fa35cc146ce30a7706
Shiloh <austyn.you@gmail.com>
2024-08-15 00:37:56 +0900

    create UserDto and LoginDto

---
+++ API/DTOs/UserDto.cs
@@ -0,0 +1,12 @@
+namespace API.DTOs
+{
+    public class UserDto
+    {
+        public string DisplayName { get; set; }
+        public string Token { get; set; }
+        public string PhotoUrl { get; set; }
+        public string Username { get; set; }
+        public string Gender { get; set; }
+        public string Native { get; set; }
+    }
+}
