f7f5698506fef94a4d71db4ea28b1ce53b9d202f
Shiloh <austyn.you@gmail.com>
2024-08-15 00:22:33 +0900

    identity and create database with see users

---
+++ Persistence/DataContext.cs
@@ -0,0 +1,14 @@
+using Domain;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence
+{
+    public class DataContext : IdentityDbContext<AppUser>
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+
+    }
+} 
