56c1528ae353b9749d6b84f932401131e7ca9119
Shiloh <austyn.you@gmail.com>
2024-08-16 00:00:58 +0900

    update seed data

--- Persistence/Seed.cs
+++ Persistence/Seed.cs
@@ -1,38 +1,23 @@
+using System.Text.Json;
 using Domain;
 using Microsoft.AspNetCore.Identity;
 
 namespace Persistence
 {
-  public class Seed
+    public class Seed
     {
         public static async Task SeedData(DataContext context, UserManager<AppUser> userManager)
         {
             if (!userManager.Users.Any())
             {
-                var users = new List<AppUser>
-                {
-                    new AppUser
-                    {
-                        DisplayName = "Bob",
-                        UserName = "bob",
-                        Email = "bob@test.com"
-                    },
-                    new AppUser
-                    {
-                        DisplayName = "Jane",
-                        UserName = "jane",
-                        Email = "jane@test.com"
-                    },
-                    new AppUser
-                    {
-                        DisplayName = "Tom",
-                        UserName = "tom",
-                        Email = "tom@test.com"
-                    },
-                };
+                var userData = await File.ReadAllTextAsync("UserSeedData.json");
+                var users = JsonSerializer.Deserialize<List<AppUser>>(userData);
 
                 foreach (var user in users)
                 {
+                    user.UserName = user.UserName.ToLower();
+                    user.Created = DateTime.SpecifyKind(user.Created, DateTimeKind.Utc);
+                    user.LastActive = DateTime.SpecifyKind(user.LastActive, DateTimeKind.Utc);
                     await userManager.CreateAsync(user, "Pa$$w0rd");
                 }
             }  
