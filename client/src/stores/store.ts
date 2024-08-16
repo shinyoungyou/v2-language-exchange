0bf0839927f02a665e23837d5b2aa12f2d3e1427
shinyoungyou <austyn.you@gmail.com>
2024-08-16 21:52:47 +0900

    load members with pagination and user params

--- client/src/stores/store.ts
+++ client/src/stores/store.ts
@@ -2,17 +2,20 @@ import { createContext, useContext } from "react";
 import CommonStore from "./commonStore";
 import userStore from "./userStore";
 import ModalStore from "./modalStore";
+import MemberStore from './memberStore';
 
 interface Store {
   commonStore: CommonStore;
   userStore: userStore;
   modalStore: ModalStore;
+  memberStore: MemberStore;
 }
 
 export const store: Store = {
   commonStore: new CommonStore(),
   userStore: new userStore(),
-  modalStore: new ModalStore()
+  modalStore: new ModalStore(),
+  memberStore: new MemberStore(),
 };
 
 export const StoreContext = createContext(store);
