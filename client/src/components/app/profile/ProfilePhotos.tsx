feeae6e85c140ea920f5b7c5064589d8d90cba0f
shinyoungyou <austyn.you@gmail.com>
2024-08-20 19:59:34 +0900

    markup the photo editor interface

--- client/src/components/app/profile/ProfilePhotos.tsx
+++ client/src/components/app/profile/ProfilePhotos.tsx
@@ -1,9 +1,9 @@
-import { Member } from "@/models/member";
+import { Member, Photo } from "@/models/member";
 import { observer } from "mobx-react-lite";
 import { Header, Image, Icon, Button } from "semantic-ui-react";
-// import { SyntheticEvent, useEffect, useState } from "react";
+import { SyntheticEvent, useState } from "react";
 import { useStore } from "@/stores/store";
-// import AddPhotoForm from "../../common/imageUpload/AddPhotoForm";
+import AddPhotoForm from "@/components/common/imageUpload/AddPhotoForm";
 
 interface Props {
   member: Member;
@@ -11,24 +11,21 @@ interface Props {
 
 export default observer(function ProfilePhotos({ member }: Props) {
   const {
-    // modalStore,
-    memberStore: { isCurrentUser, 
-      // setMainPhoto, deletePhoto, 
-      // loading 
-    },
+    modalStore,
+    memberStore: { isCurrentUser, setMainPhoto, deletePhoto, loading },
   } = useStore();
 
-  // const [target, setTarget] = useState('');
+  const [target, setTarget] = useState('');
 
-  // function handleSetMain(photo: Photo, e: SyntheticEvent<HTMLButtonElement>) {
-  //   setTarget(e.currentTarget.name);
-  //   setMainPhoto(photo);
-  // }
+  function handleSetMain(photo: Photo, e: SyntheticEvent<HTMLButtonElement>) {
+    setTarget(e.currentTarget.name);
+    setMainPhoto(photo);
+  }
 
-  // function handleDeletePhoto(photo: Photo, e: SyntheticEvent<HTMLButtonElement>) {
-  //   setTarget(e.currentTarget.name);
-  //   deletePhoto(photo);
-  // }
+  function handleDeletePhoto(photo: Photo, e: SyntheticEvent<HTMLButtonElement>) {
+    setTarget(e.currentTarget.name);
+    deletePhoto(photo);
+  }
 
   return (
     <div className="profilePhotos">
@@ -46,14 +43,16 @@ export default observer(function ProfilePhotos({ member }: Props) {
               <Button 
                 name={p.id} 
                 // loading={target === p.id && loading}
-                // onClick={e => handleSetMain(p, e)}
+                disabled={p.isMain}
+                onClick={e => handleSetMain(p, e)}
                 icon>
-                <Icon name="star" color="blue" disabled={p.isMain} />
+                <Icon name="star" color="blue" />
               </Button>
               <Button
                 name={p.id} 
+                disabled={p.isMain}
                 // loading={target === p.id && loading}
-                // onClick={e => handleDeletePhoto(p, e)}
+                onClick={e => handleDeletePhoto(p, e)}
                 icon>
                 <Icon name="delete" color="red" />
               </Button>
@@ -63,7 +62,7 @@ export default observer(function ProfilePhotos({ member }: Props) {
         {isCurrentUser && (
           <Image>
             <button
-              // onClick={() => modalStore.openModal(<AddPhotoForm />)}
+              onClick={() => modalStore.openModal(<AddPhotoForm />)}
               className="addPhotoButton"
             >
               <Icon name="plus" color="grey" size="large" />
