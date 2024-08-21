088032186aed560b2ac2dee3aff4b1aecee63283
shinyoungyou <austyn.you@gmail.com>
2024-08-21 15:27:29 +0900

    create Message, Group, Connection tables

---
+++ Domain/Message.cs
@@ -0,0 +1,18 @@
+namespace Domain
+{
+    public class Message
+    {
+        public int Id { get; set; }
+        public string SenderId { get; set; }
+        public string SenderUsername { get; set; }
+        public AppUser Sender { get; set; }
+        public string RecipientId { get; set; }
+        public string RecipientUsername { get; set; }
+        public AppUser Recipient { get; set; }
+        public string Content { get; set; }
+        public DateTime? DateRead { get; set; }
+        public DateTime MessageSent { get; set; } = DateTime.UtcNow;
+        public bool SenderDeleted { get; set; }
+        public bool RecipientDeleted { get; set; }
+    }
+}
--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -11,9 +11,24 @@ namespace Persistence
         }
 
         public DbSet<Photo> Photos { get; set; }
+        public DbSet<Message> Messages { get; set; }
+        public DbSet<Group> Groups { get; set; }
+        public DbSet<Connection> Connections { get; set; }
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
+
+            builder.Entity<Message>()
+                .HasOne(s => s.Sender)
+                .WithMany(m => m.MessagesSent)
+                .HasForeignKey(s => s.SenderId)
+                .OnDelete(DeleteBehavior.Restrict);
+      
+            builder.Entity<Message>()
+                .HasOne(r => r.Recipient)
+                .WithMany(m => m.MessagesReceived)
+                .HasForeignKey(s => s.RecipientId)
+                .OnDelete(DeleteBehavior.Restrict);
         }
     }
 } 
